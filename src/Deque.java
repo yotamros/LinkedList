34fb39539613dc7c03487c8c44d2f7e75868f627
Yotam Rosenbaum <yotamrosen@gmail.com>
2014-07-28 15:22:16 -0500

    Add iterator.

--- src/Deque.java
+++ src/Deque.java
@@ -12,6 +12,7 @@ public class Deque<Item> implements Iterable<Item> {
         Node prev;
     }
     
+    
    /**
     * Is the deque empty?
     * @return
@@ -94,28 +95,48 @@ public class Deque<Item> implements Iterable<Item> {
     * Return an iterator over items in order from front to end
     * 
     */
-   public Iterator<Item> iterator() {
-       return null;
+   public Iterator<Item> iterator() { 
+       return new ListIterator(); 
    }
    
+   private class ListIterator implements Iterator<Item> {
+       
+       private Node current = first;
+       
+       public boolean hasNext() { 
+           return current != null; 
+       }
+       
+       public void remove() { 
+           /* not supported */ 
+           throw new UnsupportedOperationException("Remove method is not supported");
+       } 
+       
+       public Item next() {
+           if ( current.item == null) {
+               throw new java.util.NoSuchElementException("There are no more items in the list");
+           }
+           Item item = current.item;
+           current = current.next; 
+           return item;
+       }
+   }
+   
+   private void printItemsFromBeginning() {
+       for (int i=0; i<size; i++) {
+           System.out.println(first.item);
+           first = first.next;
+       }
+   }
+   
+   private void printItemsFromEnd() {
+       for (int i=0; i<size; i++) {
+           System.out.println(last.item);
+           last = last.prev;
+       }
+   }
    
    public static void main(String[] args) {
        Deque<String> test = new Deque<String>();
-       System.out.println(test.isEmpty());
-       test.addFirst("jojo");
-       test.addFirst("momo");
-       System.out.println(test.isEmpty());
-       System.out.println("items in list: " +test.size);
-       test.addLast("koko");
-       System.out.println("items in list: " +test.size);
-       System.out.println(test.removeFirst());
-       System.out.println("items in list: " +test.size);
-       System.out.println(test.removeLast());
-       System.out.println("items in list: " +test.size);
-       test.removeFirst();
-       System.out.println("items in list: " +test.size);
-       test.removeFirst();
-
    }
-   
 }
